ab404394c8e5fe12ed9c22e6510ac9f610f9b307
TariqCO <tariq.official1712@gmail.com>
2026-01-06 12:36:57 +0500

    First Python in You Excel

---
+++ hellopy.py
@@ -0,0 +1,56 @@
+# Types of funcutions in python!
+# 
+# - Buit in Functions
+# - User Defined Functions
+
+
+
+print # to print any display any statment or value
+
+# logical operators:
+#     and = works when both conditions are true
+#     or = works when any condition is true
+#     not = works when both are false
+#     
+#  membership operators:
+#      is
+#
+
+
+print("Hello World..")
+print(1+1)
+print("1"+ "2")
+print(" 1: Hello this is Tariq learning basics about python \n 2: I love to code and develop things \n 3: Learning Web dev in python in You Excel \n 4: My Hobbies to play Football Cricket")
+
+hello = 12
+
+print(hello)
+
+print(hello + 2)
+
+print("Hello is equal to: " , hello)
+
+
+num1 = 260
+num2 = 100
+
+print("\n This is Addition: ", num1 + num2 ,"\n This is subtraction: ", num1 - num2 , "\n This is multiplication: " ,num1 * num2, " \n This is division: ", num1 / num2 , "\n This is Reminder: ", num1 % num2)
+
+
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1 / num2
+remind = num1 % num2
+
+print("\n This is Addition: ", add ,"\n This is subtraction: ", sub , "\n This is multiplication: " ,multi, " \n This is division: ", div , "\n This is Reminder: ", remind , "\n")
+
+
+
+# input / output in python!
+num1 = int(input("Enter a number 1: "))
+num2 = int(input("Enter a number 2: "))
+
+
+print("This is the total of num1 and num2: " , num1 + num2)
+
